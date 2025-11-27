886a776e4854257185e660a2496041a424eefcaf
Longtui Jia <Longtui.Jia@analog.com>
2025-11-27 16:36:46 +0900

    11/26

--- Array/binarySearch.py
+++ Array/binarySearch.py
@@ -3,22 +3,22 @@
 from typing import List
 
 
-class Solution:
-    def binary_search(self,nums:List[int], target:int)->int:
-        left=0
-        right= len(nums) - 1
-        while left<=right:
-            mid = left + (right - left)//2
-            if nums[mid]>target:
-                right = mid - 1
-            elif nums[mid]<target:
-                left = mid + 1
-            else:
-                return mid
-        return -1
+def binary_search(nums:List[int], target:int)->int:
+    left=0
+    right= len(nums) - 1
+    while left<=right:
+        mid = left + (right - left)//2
+        if nums[mid]>target:
+            right = mid - 1
+        elif nums[mid]<target:
+            left = mid + 1
+        else:
+            return mid
+    return -1
+
+
 
 if __name__ == "__main__":
     nums = [-9,-2,-1,0,1,4,6,10,17]
     target = 4
-    s=Solution()
-    print(s.binary_search(nums, target))
+    print(binary_search(nums, target))
