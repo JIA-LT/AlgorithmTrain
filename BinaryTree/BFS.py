21b20ea6b18cfc437e37ba1f7892fda932690189
Lucas <jialongtui@gmail.com>
2025-12-03 20:59:06 +0900

    12/3

--- BinaryTree/BFS.py
+++ BinaryTree/BFS.py
@@ -21,7 +21,7 @@ def bfs(root:Optional[TreeNode])->List[List[int]]:
         result.append(level)
     return result
 
-
+# 199
 def right_side_view(root: Optional[TreeNode])->List[List[int]]:
     if not root:
         return []
@@ -39,11 +39,45 @@ def right_side_view(root: Optional[TreeNode])->List[List[int]]:
                 queue.append(node.right)
     return right_side_view
 
+# 104
+def max_level(root: Optional[TreeNode])->int:
+    if not root:
+        return 0
+    depth = 0
+    queue = deque([root])
 
-    # 199
+    while queue:
+        depth += 1
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+    return depth
+
+def min_level(root: Optional[TreeNode])->int:
+    if not root:
+        return 0
+    queue = deque([root])
+    min_level = 0
+    while queue:
+        min_level += 1
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            if not node.left and not node.right:
+                return min_level
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+    return min_level
 
 if __name__ == '__main__':
-    array = [5,3,9,4,6,15,7]
+    array = [5,3,9,None,None,15,7]
     root = build_tree_by_array(array)
     print(bfs(root))
     print(right_side_view(root))
+    print(max_level(root))
+    print(min_level(root))
+
